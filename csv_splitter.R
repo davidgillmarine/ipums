f7256bd6c1d64b570f213fa1e088713634a10581
davidgillmarine <gilldavida@gmail.com>
2017-11-13 17:01:16 -0500

    many changes, but this code works for all 82 countries. May need to do QC on the output

--- csv_splitter.R
+++ csv_splitter.R
@@ -6,21 +6,18 @@ sum2=function(x){sum(x,na.rm=TRUE)}
 mean2=function(x){mean(x,na.rm=TRUE)}
 # set this to the maximum amount of working memory you need (WARNING: ensure you have enough space on your HDD)
 memory.limit(size = 32000) 
-
-# Unzip files 
-zip.loc <- "E:/My Documents/Important Files/Work/SESYNC/Data analysis/IPUMS/data"
+#zip.loc <- "E:/My Documents/Important Files/Work/SESYNC/Data analysis/IPUMS/data"
 unzip.loc <- "E:/My Documents/Important Files/Work/SESYNC/Data analysis/IPUMS/data/downloads"
 loc_files <- "E:/My Documents/Important Files/Work/SESYNC/Data analysis/IPUMS/data/ipumsi_countries/"
 
-file.list<- list.files(zip.loc,pattern="*.gz",recursive = T, full.names = T)
-for (i in 1:length(file.list)){
-  gunzip(file.list[i],overwrite=T)
-}
+#-- Unzip files 
+#file.list<- list.files(zip.loc,pattern="*.gz",recursive = T, full.names = T)
+#for (i in 1:length(file.list)){
+#  gunzip(file.list[i],overwrite=T)
+#}
 
-# Create separate csv files 
+#-- Create separate csv files 
 file.list<- list.files(unzip.loc,recursive = T, full.names = T)
-
-
 # loop through the files and write a file from each country each country in each file 
 for (i in 1:length(file.list)){
   ptm <- proc.time()               # to figure out how long it takes to run the function
@@ -41,7 +38,7 @@ for (i in 1:length(file.list)){
   proc.time() - ptm
   }
 
-# get list of countries included
+#-- get list of countries included
 output.list<- list.files('E:/My Documents/Important Files/Work/SESYNC/Data analysis/IPUMS/data/ipumsi_countries',recursive = T, full.names = F)
 output.list <- gsub('ipums_|.csv','',output.list)
 output.list <-as.numeric(output.list)
